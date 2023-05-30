fcaf3efaf0d5e2f666c556df2c7e9b9c9b61f909
ChangCheWei <10656004@ntub.edu.tw>
2023-05-31 01:53:03 +0800

    Create Data.java
    Using class Data can quickly making ResponseEntity by getResponse method

---
+++ src/main/java/com/example/sajava/Data.java
@@ -0,0 +1,48 @@
+package com.example.sajava;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Data {
+    final String s = "statusCode", r = "response";
+    private int code;
+    private Object data;
+    private Map<String, Object> response;
+
+    public Data(){
+        this(404, null);
+    }
+    public Data(int code, Object data){
+        this.code = code;
+        this.data = data;
+    }
+
+    public int getCode() {
+        return code;
+    }
+    public Object getData() {
+        return data;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public ResponseEntity<Map<String, Object>> getResponse(){
+        response = new LinkedHashMap<>();
+        response.put(s, code);
+
+        if(code/100 == 2) {
+            response.put(r, data);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        else return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+
+    }
+}
