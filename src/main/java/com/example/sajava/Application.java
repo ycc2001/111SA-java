5aa9b440361ba01e0e59751ec628709cf067804a
ChangCheWei <10656004@ntub.edu.tw>
2023-05-31 01:52:39 +0800

    Create connecting database files

--- src/main/java/com/example/sajava/Application.java
+++ src/main/java/com/example/sajava/Application.java
@@ -2,9 +2,14 @@ package com.example.sajava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class Application {
+@ComponentScan({"com.example", "com.example.sajava", "com.example.sajava.controller", "com.example.sajava.model", "com.example.sajava.repository", "com.example.sajava.service"})
+@EntityScan({"com.example", "com.example.sajava", "com.example.sajava.controller", "com.example.sajava.model", "com.example.sajava.repository", "com.example.sajava.service"})
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
